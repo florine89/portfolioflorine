1aff673a5a12b9295208e785b23b50d22e16fd84
florine89 <114070434+florine89@users.noreply.github.com>
2023-04-28 15:25:56 +0200

    doublepageaccueil

---
+++ components/Navbar.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { BsFillCloudMoonFill, BsLinkedin, BsGithub, BsFillEnvelopeAtFill } from 'react-icons/bs';
+
+
+const Navbar = () => {
+    return (
+        <div className='fixed w-full h-20 shadow-xl z-[100]'>
+            <nav className="flex justify-between items-center w-full px-2 2xl:px-16 ">
+                <ul className='hidden md:flex'>
+                    <Link href='/'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>
+                            Home
+                        </li>
+                    </Link>
+                    <Link href='/'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>
+                            About
+                        </li>
+                    </Link>
+                    <Link href='/'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>
+                            Skills
+                        </li>
+                    </Link>
+                    <Link href='/'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>
+                            Projects
+                        </li>
+                    </Link>
+                    <Link href='/'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>
+                            Contact me
+                        </li>
+                    </Link>
+                </ul>
+                <div className='md:hidden'>
+                    <AiOutlineMenu size={25} />
+                </div>
+                <ul className='flex items-center'>
+                    <li>
+                        <BsFillCloudMoonFill className='cursor-pointer text-3xl' />
+                    </li>
+                    <li className='hover:scale-125 ease-in duration-300 mr-10'><a href="#" className='bg-gradient-to-r from-pink-200 to-pink-800 text-white px-4 py-2 rounded-lg ml-8 scale-50 '>Resume</a></li>
+                </ul>
+            </nav>
+
+            <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+
+                <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+
+                    <div className='flex w-full items-center justify-between'>
+                        <div className='font-burtons text-pink-700'>
+                            Florine's Portfolio
+                        </div>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                            <AiOutlineClose />
+                        </div>
+                    </div>
+
+                    <div className='border-b border-gray-300 my-4'>
+                        <p className='w-[85%] md:w-[90%] py-4'>Lest's work together !</p>
+                    </div>
+
+                    <div py-4 flex flex-col>
+                        <ul className='uppercase'>
+                            <Link href='/'>
+                                <li className='py-4 text-sm'>Home</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className='py-4 text-sm'>About</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className='py-4 text-sm'>Skills</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className='py-4 text-sm'>Projects</li>
+                            </Link>
+                            <Link href='/'>
+                                <li className='py-4 text-sm'>Contact me</li>
+                            </Link>
+                        </ul>
+                    </div>
+
+                    <div className='pt-20 '>
+
+                        <p className='uppercase tracking-widest text-pink-700'>Let's connect</p>
+
+                        <div className='flex justify-center m-5 rounded-full shadow-lg cursor-pointer'>
+                            <BsLinkedin size={20} />
+                        </div>
+
+                        <div className='flex justify-center m-5 rounded-full shadow-lg cursor-pointer'>
+                            <BsGithub size={20} />
+                        </div>
+
+                        <div className='flex justify-center m-5 rounded-full shadow-lg cursor-pointer'>
+                            <BsFillEnvelopeAtFill size={20}/>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head';
-import { BsFillCloudMoonFill, BsLinkedin, BsGithub } from 'react-icons/bs';
+import { BsFillCloudMoonFill, BsLinkedin, BsGithub, BsFillArrowDownCircleFill } from 'react-icons/bs';
 import Image from "next/image";
 import me from "../../public/portfoliome.jpg"
+import Navbar from '../../components/Navbar';
 
 function Home() {
   return (
@@ -10,32 +11,32 @@ function Home() {
         <title>Florine Portfolio</title>
         <meta property="og:title" content="My page title" key="title" />
       </Head>
-      <main className=' bg-slate-100'>
-        <section className='min-h-screen'>
-          <nav className="py-10 mb-12 flex justify-between">
-            <h1 className='text-xl font-burtons ml-7'>Florine's Portfolio</h1>
-            <ul className='flex items-center'>
-              <li>
-                <BsFillCloudMoonFill className='cursor-pointer text-3xl' />
-              </li>
-              <li className='hover:scale-125 ease-in duration-300 mr-10'><a href="#" className='bg-gradient-to-r from-pink-200 to-pink-800 text-white px-4 py-2 rounded-lg ml-8 scale-50 '>Resume</a></li>
-            </ul>
-          </nav>
+      <main className=''>  
+      
+      <Navbar />
+            
           <div className='text-center p-10 py-10'>
             <h2 className='text-5xl py-2 text-pink-900 font-medium '>Florine Ferreira</h2>
-            <h3 className='text-2xl py-2'>Developer Javascript</h3>
+            <h3 className='text-2xl py-2'>Developer Fullstack Javascript</h3>
             <p className='text-md py-5 leading-8 text-gray-800'>Hello there ! </p>
             <p> Welcome to my portfolio</p>
             <p>I am a full-stack developer based in Paris. Visit my profile and stay connect with me.</p>
             <p>Let's start with some projects ! </p>
-          </div>
-          <div className='flex justify-center mt-20'>
-            <Image className='w-64  rounded-full brightness-90' src={me} />
+            <a href="#projets" className='text-5xl flex justify-center m-14 animate-bounce'>
+            <BsFillArrowDownCircleFill className='cursor-pointer' />
+            </a>
           </div>
           <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
             <BsGithub />
             <BsLinkedin />
           </div>
+          <div className='flex justify-center mt-20'>
+            <Image className='w-64  rounded-full brightness-90' src={me} />
+          </div>          
+        <section id='projets'>
+       <h3 className='text-3xl py-1'>My projects</h3>   
+       <div>
+       </div>
         </section>
       </main>
     </div>
