686e6dfb971d17884a823285b5e95d8dcd3c6fed
florine89 <114070434+florine89@users.noreply.github.com>
2023-05-02 22:11:48 +0200

    scrollUp

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
 import contact from '../public/assets/contact2.jpg';
-import { BsLinkedin, BsGithub, BsFillEnvelopeAtFill } from 'react-icons/bs';
+import { BsLinkedin, BsGithub, BsFillEnvelopeAtFill, BsChevronDoubleUp } from 'react-icons/bs';
+import Link from 'next/link'
 
 const Contact = () => {
     return (
@@ -16,25 +17,30 @@ const Contact = () => {
                             <div>
                                 <Image className='rounded-xl hovers:scale-105 ease-in duration-300' src={contact} alt='/' />
                             </div>
-                            
+
                             <h2 className='py-2 sm: text-s lg: text-2xl'>Florine Ferreira</h2>
-                            
+
                             <p>Front-End Developer</p>
                             <p className='py-4'>Let's talk !</p>
-                           
+
                             <p className='uppercase pt-8'>Connect with me</p>
                             <div className='flex items-center justify-between py-4'>
-                               
+
                                 <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 '>
                                     <BsGithub className='cursor-pointer hover:scale-110 ease-in duration-300' />
                                     <BsLinkedin className='cursor-pointer hover:scale-110 ease-in duration-300' />
                                     <BsFillEnvelopeAtFill className='cursor-pointer hover:scale-110 ease-in duration-300' />
                                 </div>
-                             
-                                </div>
-                            
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div className='flex justify-center py-16'>
+                    <Link href=''>
+                    <div className='rounded-full shadow-xl cursor-pointer hover:scale-110 ease-in duration-300'>
+<BsChevronDoubleUp className='text-4xl text-gray-400' />
+                    </div>
+                    </Link>
                 </div>
             </div>
         </div>
